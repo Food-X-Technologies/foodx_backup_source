184accd54b3d2347eb747467b5b6ef23e6c6942b
Russell Smiley <russell@foodxtech.com>
2022-02-14 14:21:23 +0000

    renamed python package

--- backup_source/__init__.py
+++
@@ -1,7 +0,0 @@
-#  Copyright (c) 1700 My Name
-#
-#  This file is part of start.
-
-"""A project that does something."""
-
-from ._version import __version__  # noqa: F401
---
+++ foodx_backup_source/__init__.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 1700-2022 Food-X Technologies
+#
+#  This file is part of foodx_backup_source.
+#
+#  You should have received a copy of the MIT License along with
+#  foodx_backup_source. If not, see <https://opensource.org/licenses/MIT>.
+
+"""A backup utility for FoodX source code."""
+
+from ._version import __version__  # noqa: F401
