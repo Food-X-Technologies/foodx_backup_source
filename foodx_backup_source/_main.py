987867855a7eafff040e8a6fb2510a011f01a910
Russell Smiley <russell@foodxtech.com>
2022-02-22 21:44:15 +0000

    implemented --git-ref option

--- foodx_backup_source/_main.py
+++ foodx_backup_source/_main.py
@@ -28,6 +28,8 @@ from ._snapshot import do_snapshot
 
 DEFAULT_OUTPUT_PATH = pathlib.Path(".")
 
+GitReferences = typing.Dict[str, str]
+
 
 def _strip_paths(tarinfo: tarfile.TarInfo) -> tarfile.TarInfo:
     """Ensure source filesystem absolute paths are not reflected in tar file."""
@@ -42,14 +44,29 @@ def _isoformat_now() -> str:
     return datetime.datetime.utcnow().isoformat()[:-3] + "Z"
 
 
-async def _gather(
+def _apply_user_refs(
+    data: BackupDefinitions, git_refs: GitReferences
+) -> BackupDefinitions:
+    if git_refs:
+        names = git_refs.keys()
+        for x in data:
+            if x.name in names:
+                x.configuration.release.ref = git_refs[x.name]
+
+    return data
+
+
+async def _launch_packaging(
     project_name: str,
     project_directory: pathlib.Path,
     output_directory: pathlib.Path,
     token: typing.Optional[str],
+    git_refs: GitReferences,
 ) -> None:
     files: PathSet = discover_backup_definitions(project_directory)
     data: BackupDefinitions = await load_backup_definitions(files)
+
+    data = _apply_user_refs(data, git_refs)
 
     with tempfile.TemporaryDirectory() as d:
         archive_directory = pathlib.Path(d)
@@ -86,6 +103,17 @@ async def _gather(
     ),
 )
 @click.option(
+    "--git-ref",
+    default=None,
+    help="""Specify a git reference for the named repo.
+
+The reference is specified in the form `<name>=<gitref>` where the name is
+an entry in the dependencies YAML files acquired from PROJECT_DIRECTORY.
+""",
+    multiple=True,
+    type=str,
+)
+@click.option(
     "--token-file",
     default=None,
     help="""Personal access token for authenticating against repositories.
@@ -99,15 +127,39 @@ def main(
     project_name: str,
     project_directory: pathlib.Path,
     output_dir: pathlib.Path,
+    git_ref: typing.Optional[typing.List[str]],
     token_file: typing.Optional[io.TextIOBase],
 ) -> None:
-    """Primary entry point and execution path of backup utility."""
+    """
+    Package repositories for archiving.
+
+    Repositories specified in YAML "dependencies" files are packaged into a
+    gzipped tar file at the specified git commit reference and then the
+    collection of tar files is packaged into a single gzipped tar file for
+    archiving. A sha256sum file for each tar package is also generated.
+    """
     try:
         token_value = None
         if token_file:
             token_value = token_file.read().strip()
+
+        processed_refs: GitReferences = dict()
+        if git_ref:
+            for x in git_ref:
+                tokens = x.split("=")
+                if len(tokens) != 2:
+                    raise RuntimeError(f"Malformed git ref option, {x}")
+
+                processed_refs[tokens[0].strip()] = tokens[1].strip()
+
         asyncio.run(
-            _gather(project_name, project_directory, output_dir, token_value)
+            _launch_packaging(
+                project_name,
+                project_directory,
+                output_dir,
+                token_value,
+                processed_refs,
+            )
         )
     except KeyboardInterrupt:
         click.echo("User aborted execution. Exiting.")
--- tests/ci/unit_tests/test_main.py
+++ tests/ci/unit_tests/test_main.py
@@ -12,15 +12,18 @@ import click
 import pytest
 from click.testing import CliRunner
 
-import foodx_backup_source._main
 from foodx_backup_source._file_io import BackupDefinitions
-from foodx_backup_source._main import DEFAULT_OUTPUT_PATH, _gather, main
+from foodx_backup_source._main import (
+    DEFAULT_OUTPUT_PATH,
+    _launch_packaging,
+    main,
+)
 from foodx_backup_source.schema import ApplicationDefinition, DependencyFile
 
 
 @pytest.fixture()
 def mock_gather(mocker):
-    return mocker.patch("foodx_backup_source._main._gather")
+    return mocker.patch("foodx_backup_source._main._launch_packaging")
 
 
 @pytest.fixture()
@@ -42,7 +45,7 @@ def mock_path(mocker):
 
 
 @pytest.fixture()
-def mock_definitions(load_yaml_content):
+def mock_definitions(load_yaml_content, mocker):
     content_text = """
 ---
 context:
@@ -65,19 +68,44 @@ context:
         )
         definitions.append(this_element)
 
-    return definitions
+    mocker.patch(
+        "foodx_backup_source._main.load_backup_definitions",
+        return_value=definitions,
+    )
 
 
-class TestGather:
+class TestLaunchPackaging:
     @pytest.mark.asyncio
     async def test_clean(self, mock_definitions, mocker):
         mock_snapshot = mocker.patch("foodx_backup_source._main.do_snapshot")
         mocker.patch("foodx_backup_source._main.tarfile.open")
         mocker.patch("foodx_backup_source._main.discover_backup_definitions")
         mocker.patch(
-            "foodx_backup_source._main.load_backup_definitions",
-            return_value=mock_definitions,
+            "foodx_backup_source._main._isoformat_now", return_value="today"
         )
+        mock_hash_file = mocker.patch(
+            "foodx_backup_source._main.create_hash_file"
+        )
+        arguments = {
+            "project_name": "this_project",
+            "project_directory": pathlib.Path("some/project"),
+            "output_directory": pathlib.Path("some/output"),
+            "git_refs": dict(),
+            "token": None,
+        }
+
+        await _launch_packaging(**arguments)
+
+        mock_hash_file.assert_called_once_with(
+            pathlib.Path("some/output/this_project-today.tar.gz")
+        )
+        mock_snapshot.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_git_ref(self, mock_definitions, mocker):
+        mock_snapshot = mocker.patch("foodx_backup_source._main.do_snapshot")
+        mocker.patch("foodx_backup_source._main.tarfile.open")
+        mocker.patch("foodx_backup_source._main.discover_backup_definitions")
         mocker.patch(
             "foodx_backup_source._main._isoformat_now", return_value="today"
         )
@@ -89,14 +117,18 @@ class TestGather:
             "project_directory": pathlib.Path("some/project"),
             "output_directory": pathlib.Path("some/output"),
             "token": None,
+            "git_refs": {"r1": "abc123"},
         }
 
-        await _gather(**arguments)
+        await _launch_packaging(**arguments)
 
         mock_hash_file.assert_called_once_with(
             pathlib.Path("some/output/this_project-today.tar.gz")
         )
         mock_snapshot.assert_called_once()
+        assert (
+            mock_snapshot.call_args[0][0].configuration.release.ref == "abc123"
+        )
 
 
 class TestMain:
@@ -114,6 +146,7 @@ class TestMain:
             pathlib.Path(arguments[1]),
             DEFAULT_OUTPUT_PATH,
             None,
+            dict(),
         )
 
     def test_token_file_stdin(
@@ -134,6 +167,7 @@ class TestMain:
             mocker.ANY,
             mocker.ANY,
             "deadb33f",
+            mocker.ANY,
         )
 
     def test_token_file_whitespace(
@@ -154,6 +188,7 @@ class TestMain:
             mocker.ANY,
             mocker.ANY,
             "deadb33f",
+            mocker.ANY,
         )
 
     def test_output(self, mock_gather, mock_runner, mock_path, mocker):
@@ -172,4 +207,47 @@ class TestMain:
             mocker.ANY,
             pathlib.Path("output/dir"),
             mocker.ANY,
+            mocker.ANY,
+        )
+
+    def test_git_ref(self, mock_gather, mock_runner, mock_path, mocker):
+        arguments = [
+            "this_project",
+            "some/path",
+            "--git-ref",
+            "r1=abc123",
+        ]
+
+        result = mock_runner.invoke(main, arguments)
+
+        assert result.exit_code == 0
+        mock_gather.assert_awaited_once_with(
+            mocker.ANY,
+            mocker.ANY,
+            mocker.ANY,
+            mocker.ANY,
+            {"r1": "abc123"},
+        )
+
+    def test_multiple_git_ref(
+        self, mock_gather, mock_runner, mock_path, mocker
+    ):
+        arguments = [
+            "this_project",
+            "some/path",
+            "--git-ref",
+            "r1=abc123",
+            "--git-ref",
+            "r3=123abc",
+        ]
+
+        result = mock_runner.invoke(main, arguments)
+
+        assert result.exit_code == 0
+        mock_gather.assert_awaited_once_with(
+            mocker.ANY,
+            mocker.ANY,
+            mocker.ANY,
+            mocker.ANY,
+            {"r1": "abc123", "r3": "123abc"},
         )
